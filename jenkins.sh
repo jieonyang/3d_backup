ba7410f019e8a62350d5dd2edb0c7de4c0a043bb
jieonyang <7ieon.yang@gmail.com>
2022-04-15 04:55:28 +0000

    first commit

---
+++ jenkins.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+kubectl create namespace admin
+
+my_git=`git config --list | grep user.name | cut -d "=" -f2`
+
+argocd app create jenkins \
+--repo https://github.com/$my_git/emarket_PaC.git \
+--path jenkins \
+--dest-server https://kubernetes.default.svc \
+--dest-namespace admin
+
+argocd app sync jenkins
+
+
+echo "apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: jenkins-agent
+  namespace: admin
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: jenkins-agent
+  namespace: admin
+rules:
+- apiGroups:
+  - \"\"
+  resources:
+  - pods
+  verbs:
+  - create
+  - delete
+  - get
+  - list
+  - patch
+  - update
+  - watch
+- apiGroups:
+  - \"\"
+  resources:
+  - pods/exec
+  verbs:
+  - create
+  - delete
+  - get
+  - list
+  - patch
+  - update
+  - watch
+- apiGroups:
+  - \"\"
+  resources:
+  - pods/log
+  verbs:
+  - get
+  - list
+  - watch
+- apiGroups:
+  - \"\"
+  resources:
+  - events
+  verbs:
+  - get
+  - list
+  - watch
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: jenkins-agent
+  namespace: admin
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: jenkins-agent
+subjects:
+- kind: ServiceAccount
+  name: jenkins-agent" > jenkins-agent-sa.yaml
+
+kubectl apply -f jenkins-agent-sa.yaml
+
+echo
+echo 'Jenkins is being installed. Please wait...'
+sleep 15s
+
+echo -n '>>> Jenkins URL : '
+kubectl get svc -n admin | awk '{print $4":8080"}' | grep aws
+echo
+echo '>>> Jenkins User : admin '
+echo -n '>>> Jenkins Password : ' &&  kubectl -n admin get secret jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 -d; echo
+echo
+
+
+rm jenkins-agent-sa.yaml
+
